642b0abc88857ac155883b11e5e8ee5006927da5
Pier Mario Capra <piermariocapra@Pieros-MacBook-Pro.local>
2021-08-22 14:06:40 +0300

    registration user and member page

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa1ih8bozzTZJC2R5d7',
+        issuer: 'https://dev-33187329.okta.com/oauth2/default ',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
