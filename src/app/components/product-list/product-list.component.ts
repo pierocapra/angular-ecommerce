1a12d1c86388793d9b514154f91026a3b3278e57
Pier Mario Capra <piermariocapra@Pieros-MacBook-Pro.local>
2021-09-01 14:47:10 +0300

    responsivity piero

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -21,7 +21,7 @@ export class ProductListComponent implements OnInit {
   
   //new properties for pagination
   thePageNumber:number = 1;
-  thePageSize:number = 5;
+  thePageSize:number = 4;
   theTotalElements :number = 0;
 
   previousKeyword: string | null = null;
